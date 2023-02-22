a0f09635aa2e97ef4cc33d36bff6077940995479
shraja7 <shraja7@gmail.com>
2023-02-22 09:56:53 -0600

    implement swiper api

--- js/script.js
+++ js/script.js
@@ -233,6 +233,53 @@ if(type === 'movie') {
  }
 
 
+ //display slider movies
+ const displaySlider = async () => { 
+    const {results } = await fetchAPIData('movie/now_playing');
+
+    results.forEach(movie =>{
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+
+        div.innerHTML = `
+
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt=${movie.title} />
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+        </h4>
+      `
+
+        document.querySelector('.swiper-wrapper').appendChild(div);
+        initSwiper()
+    })
+  }
+
+  const initSwiper = () => { 
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            500:{
+                slidesPerView: 2,
+            },
+            700:{
+                slidesPerView: 3,
+            },
+            1200:{
+                slidesPerView: 4,
+            },
+        }
+    })
+   }
+
 //fetch data from TMDB API
 const fetchAPIData = async (endpoint) => {
     const API_KEY = '01e6aa79cdd7066d6cd557877ab7ac77'
@@ -278,6 +325,7 @@ const init = () => {
     switch (global.currentPage) {
         case '/': 
         case '/index.html':
+        displaySlider();
         displayPopularMovies();
         break;
         case '/shows.html':
